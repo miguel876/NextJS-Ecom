b563fc376c5d82a5c59a5e944187c8f748a1888b
Miguel <miguel87666@gmail.com>
2024-12-29 00:29:18 +0000

    feat: header changes

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -9,12 +9,28 @@ import {
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { getInitials } from '@/lib/utils';
-import DropdownMenu from './dropdown';
 import ThemeSwitch from '@/components/ui/theme-switch';
 import menus from './menus.json';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { usePathname } from 'next/navigation';
 import Cart from './cart';
+import { Menu, Settings, User, UserRound } from 'lucide-react';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { motion, AnimatePresence } from 'framer-motion';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
 
 const Header = () => {
   const { data: session } = useSession();
@@ -47,22 +63,45 @@ const Header = () => {
               scrolled ? 'shadow-md' : ' '
             } pb-2 flex justify-between items-center`}
           >
-            <Link href="/">MigStore</Link>
-            <div className="flex gap-10">
-              {menus.map((menu) => (
-                <Link href={menu.link} key={`menu-${menu.id}`}>
-                  {menu.label}
-                </Link>
-              ))}
+            <div className="flex items-center gap-2">
+              <Sheet>
+                <SheetTrigger>
+                  <Menu />
+                </SheetTrigger>
+                <SheetContent
+                  side="left"
+                  className="h-full flex flex-col justify-between"
+                >
+                  <SheetHeader>
+                    <SheetTitle />
+                    <SheetDescription>
+                      {menus.map((menu) => (
+                        <div key={`menu-${menu.id}`} className="mb-2">
+                          <Link
+                            href={menu.link}
+                            className="text-primary text-lg relative
+                              after:absolute after:bottom-0 after:left-0 after:h-[2px] 
+                              after:w-0 after:bg-primary after:transition-all after:duration-300
+                              hover:after:w-full"
+                          >
+                            {menu.label}
+                          </Link>
+                        </div>
+                      ))}
+                    </SheetDescription>
+                  </SheetHeader>
+                  <SheetFooter />
+                </SheetContent>
+              </Sheet>
+              <Link href="/">MigStore</Link>
             </div>
-            <div className="flex items-center gap-5">
-              <ThemeSwitch />
-              {!session?.user?.name ? (
-                <GoogleLogin />
-              ) : (
-                <CNDropdownMenu>
-                  <DropdownMenuTrigger>
-                    <Avatar>
+            <div className="flex items-center gap-3">
+              <Sheet>
+                <SheetTrigger>
+                  {!session?.user?.name ? (
+                    <UserRound size={20} />
+                  ) : (
+                    <Avatar className="size-7">
                       <AvatarImage
                         src={session?.user?.image as string}
                         alt="Profile Image"
@@ -71,10 +110,52 @@ const Header = () => {
                         {getInitials(session?.user?.name as string)}
                       </AvatarFallback>
                     </Avatar>
-                  </DropdownMenuTrigger>
-                  <DropdownMenu />
-                </CNDropdownMenu>
-              )}
+                  )}
+                </SheetTrigger>
+                <SheetContent>
+                  <SheetHeader>
+                    <SheetTitle>
+                      {session?.user?.name || 'Login/ Register'}
+                    </SheetTitle>
+                    <SheetDescription className="flex flex-col gap-4">
+                      {session?.user?.name ? (
+                        <div className="flex flex-col gap-2 mt-3">
+                          <Link
+                            href="/profile"
+                            className="hover:text-primary transition flex gap-1 items-center"
+                          >
+                            <UserRound size={15} />
+                            Profile
+                          </Link>
+                          <Link
+                            href="/settings"
+                            className="hover:text-primary transition flex gap-1 items-center"
+                          >
+                            <Settings size={15} />
+                            Settings
+                          </Link>
+                        </div>
+                      ) : (
+                        <>
+                          <h1>Login with your account or create an account!</h1>
+                          <GoogleLogin />
+                        </>
+                      )}
+                    </SheetDescription>
+                  </SheetHeader>
+                  <SheetFooter>
+                    {session?.user?.name && (
+                      <Button
+                        className="uppercase w-full mt-7"
+                        onClick={() => signOut()}
+                      >
+                        Logout
+                      </Button>
+                    )}
+                  </SheetFooter>
+                </SheetContent>
+              </Sheet>
+              <ThemeSwitch />
               <Cart />
             </div>
           </div>
